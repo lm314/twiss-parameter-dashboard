ae9dd3d1cc34eb18f46d596afbb809595c9b1d96
lm314 <36315128+lm314@users.noreply.github.com>
2023-02-14 16:56:41 -0700

    Add y phase space and image plots

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
 #import plotly.graph_objects as go
@@ -222,47 +223,71 @@ def transform_value(value):
 particle_dist = gen_dist_normal_coord_4d(gen_halton_gaussian_4d,num=10000)
 
 controls_style = {
-    # "position": "fixed",
-    # "top": 0,
-    # "left": 0,
-    # "bottom": 0,
+    #"position": "fixed",
+    #"top": 0,
+    #"left": 0,
+    #"bottom": 0,
     #"width": "24rem",
-    "padding": "2rem 6rem",
+    "padding": "1rem 1rem",
     #"background-color": "#f8f9fa",
     "fontSize": "2.0vh",
     "justifyContent": "center",
 }
 
-emittance_slider = dcc.Slider(0, 4, 0.1,
+emittance_slider_x = dcc.Slider(0, 3, 0.1,
     value=1,
-    id='emittance-slider',
-    marks={i: {'label': '{}'.format(10 ** i)} for i in range(0,5)})  
+    id='emittance-slider-x',
+    marks={i: {'label': None} for i in range(0,4)}) 
 
-alpha_slider = dcc.Slider(-10, 10, 0.1,
+emittance_slider_y = dcc.Slider(0, 3, 0.1,
+    value=1,
+    id='emittance-slider-y',
+    marks={i: {'label': '{}'.format(10 ** i)} for i in range(0,4)})  
+ 
+alpha_slider_x = dcc.Slider(-10, 10, 0.1,
     value=0,
-    id='alpha-slider',
-    marks= {i: {'label': f'{i}'} for i in range(-10,11,2)}) 
+    id='alpha-slider-x',
+    marks= {i: {'label': f'{i}'} for i in [-10,-5,0,5,10]}) 
 
-beta_slider = dcc.Slider(-3, 2, 0.001,
+alpha_slider_y = dcc.Slider(-10, 10, 0.1,
     value=0,
-    id='beta-slider',
+    id='alpha-slider-y',
+    marks= {i: {'label': f'{i}'} for i in [-10,-5,0,5,10]}) 
+
+beta_slider_x = dcc.Slider(-3, 2, 0.001,
+    value=0,
+    id='beta-slider-x',
     marks={i: {'label': '{}'.format(10 ** i)} for i in range(-3,3)})
 
-kinetic_energy_slider = dcc.Slider(0,4,0.1,
+beta_slider_y = dcc.Slider(-3, 2, 0.001,
+    value=0,
+    id='beta-slider-y',
+    marks={i: {'label': '{}'.format(10 ** i)} for i in range(-3,3)})
+
+kinetic_energy_slider = dcc.Slider(0,3,0.1,
     value=1,
     id='kinetic-energy-slider',
-    marks={i: {'label': '{}'.format(10 ** i)} for i in range(0,5)})
+    marks={i: {'label': '{}'.format(10 ** i)} for i in range(0,4)})
 
 controls = html.Div([
     html.Hr(),
-    dbc.Row(dbc.Col(html.P(id='emittance-output'))),
-    dbc.Row(dbc.Col(emittance_slider)),
+    dbc.Row(dbc.Col(html.P('Normalized Emittance (nm-rad)'))),
+    dbc.Row([dbc.Col(html.P(html.P(id='emittance-output-x')),width=3),
+            dbc.Col(emittance_slider_x)]),
+    dbc.Row([dbc.Col(html.P(id='emittance-output-y'),width=3),
+            dbc.Col(emittance_slider_y)]),
     html.Hr(),
-    dbc.Row(dbc.Col(html.P(id='alpha-output'))),
-    dbc.Row(dbc.Col(alpha_slider)),
+    dbc.Row(dbc.Col(html.P('\u03B1'))),
+    dbc.Row([dbc.Col(html.P(html.P(id='alpha-output-x')),width=3),
+            dbc.Col(alpha_slider_x)]),
+    dbc.Row([dbc.Col(html.P(id='alpha-output-y'),width=3),
+            dbc.Col(alpha_slider_y)]),
     html.Hr(),
-    dbc.Row(dbc.Col(html.P(id='beta-output'))),
-    dbc.Row(dbc.Col(beta_slider)),
+    dbc.Row(dbc.Col(html.P('\u03B2 (m)'))),
+    dbc.Row([dbc.Col(html.P(html.P(id='beta-output-x')),width=3),
+            dbc.Col(beta_slider_x)]),
+    dbc.Row([dbc.Col(html.P(id='beta-output-y'),width=3),
+            dbc.Col(beta_slider_y)]),
     html.Hr(),
     dbc.Row(dbc.Col(html.P(id='kinetic-energy-output'))),
     dbc.Row(dbc.Col(kinetic_energy_slider)), 
@@ -274,105 +299,216 @@ app = Dash(__name__,
            external_stylesheets=[dbc.themes.SOLAR],
            external_scripts=["https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML" ])
 
+# app.layout = html.Div([
+#     dbc.Row([
+#         dbc.Col(html.H1('Exploring Twiss Parameters'), style = {'marginLeft':'7px','marginTop':'1rem','marginBottom':'1rem','textAlign':'center'})
+#         ]),
+#     dbc.Row([
+#         dbc.Col(html.Div(dcc.Graph(id = 'graph'),style = {'marginLeft':'15px', 'marginTop':'15px', 'marginRight':'15px'}),
+#                 style={'textAlign':'center','display': 'flex',"justifyContent": "center"})
+#         ]),
+#     dbc.Row([
+#         dbc.Col(controls)
+#         ])
+# ])
+
 app.layout = html.Div([
     dbc.Row([
         dbc.Col(html.H1('Exploring Twiss Parameters'), style = {'marginLeft':'7px','marginTop':'1rem','marginBottom':'1rem','textAlign':'center'})
         ]),
     dbc.Row([
-        dbc.Col(html.Div(dcc.Graph(id = 'graph'),style = {'marginLeft':'15px', 'marginTop':'15px', 'marginRight':'15px'}),
-                style={'textAlign':'center','display': 'flex',"justifyContent": "center"})
+        dbc.Col(controls,width=3),
+        dbc.Col(html.Div(dcc.Graph(id = 'graph'),
+                         style = {'marginLeft':'15px', 'marginTop':'15px', 'marginRight':'15px'}),
+                style={'textAlign':'center','display': 'flex',"justifyContent": "center"},
+                width=9)
         ]),
-    dbc.Row([
-        dbc.Col(controls)
-        ])
 ])
 
 
 @app.callback(
     Output('graph', 'figure'),
-    Input('emittance-slider', 'value'),
-    Input('alpha-slider', 'value'),
-    Input('beta-slider', 'value'),
+    Input('emittance-slider-x', 'value'),
+    Input('emittance-slider-y', 'value'),
+    Input('alpha-slider-x', 'value'),
+    Input('alpha-slider-y', 'value'),
+    Input('beta-slider-x', 'value'),
+    Input('beta-slider-y', 'value'),
     Input('kinetic-energy-slider', 'value'))
-def update_plot(emitn,alpha,beta,kinetic_energy):
+def update_plot(emitn_x,emitn_y,alpha_x,alpha_y,beta_x,beta_y,kinetic_energy):
     
     temp_dist = particle_dist.copy()
     
-    emitn = transform_value(emitn)
-    beta = transform_value(beta)
+    # convert from log scale
+    emitn_x = transform_value(emitn_x)
+    emitn_y = transform_value(emitn_y)
+    beta_x = transform_value(beta_x)
+    beta_y = transform_value(beta_y)
     kinetic_energy = transform_value(kinetic_energy)
     
+    # get geometric emittance
     gamma0 = KE2gamma(kinetic_energy*1e6)
     beta0 = gamma2beta(gamma0)
-    emit = emitn/(beta0*gamma0);
+    emit_x = emitn_x/(beta0*gamma0);
+    emit_y = emitn_y/(beta0*gamma0);
         
     # get twiss ellipse
-    twiss_x = Twiss(alpha=alpha,beta=beta,emit=emit*1e-9)
-    x,y = ellipse_twiss(twiss_x)
-    scaleX,scaleY = list(det_plot_scale(pd.DataFrame({'x':x,'xp':y}),cutoff = 0.9).values())
+    twiss_x = Twiss(alpha=alpha_x,beta=beta_x,emit=emit_x*1e-9)
+    twiss_y = Twiss(alpha=alpha_y,beta=beta_y,emit=emit_y*1e-9)
+    x_ell_x,y_ell_x = ellipse_twiss(twiss_x)
+    x_ell_y,y_ell_y = ellipse_twiss(twiss_y)
+    scalex_x,scaley_x = list(det_plot_scale(pd.DataFrame({'x':x_ell_x,'xp':y_ell_x}),cutoff = 0.9).values())
+    scalex_y,scaley_y = list(det_plot_scale(pd.DataFrame({'y':x_ell_y,'yp':y_ell_y}),cutoff = 0.9).values())
     
     #transform particle distribution to new parameters
-    temp_dist = transform_norm_dist(temp_dist,twiss_x,twiss_x)
+    temp_dist = transform_norm_dist(temp_dist,twiss_x,twiss_y)
     
-    # fig = px.scatter(
-    #     x=x*10**-scaleX, 
-    #     y=y*10**-scaleY,
-    #     width=600, 
-    #     height=600,
-    #     render_mode="svg",
-    #     template="solar")
+    fig = make_subplots(rows=1, 
+                        cols=3,
+                        subplot_titles=("X Phase Space", "Y Phase Space", "Real-Space Image"))
+   
+    font_size_plot = 18
+    x_standoff = 0
+    y_standoff = 0
     
-    # fig.update_layout(xaxis={"title": make_phase_space_axis_labels('x',scaleX)},
-    #                   yaxis={"title": make_phase_space_axis_labels('xp',scaleY)},
-    #                   font_size=18,
-    #                   transition_duration=500)
-    
-    trace = go.Scatter(
-        x=x*10**-scaleX,
-        y=y*10**-scaleY,
+    # -------------X Phase Space--------------------------------  
+    trace_ell_x = go.Scatter(
+        x=x_ell_x*10**-scalex_x,
+        y=y_ell_x*10**-scaley_x,
         # width=600, 
         # height=600,
         # template="solar"
         marker_color='red',
+        hoverinfo='skip',
         )
     
-    trace_dist = go.Histogram2d(
-        x=temp_dist[:,0]*10**-scaleX,
-        y=temp_dist[:,1]*10**-scaleY,
+    trace_dist_x = go.Histogram2d(
+        x=temp_dist[:,0]*10**-scalex_x,
+        y=temp_dist[:,1]*10**-scaley_x,
         colorscale="Viridis",
         nbinsx=51,
-        nbinsy=51,)
+        nbinsy=51,
+        showscale=False,
+        hoverinfo='skip',)
     
-    layout = dict(xaxis={"title": make_phase_space_axis_labels('x',scaleX)},
-                  yaxis={"title": make_phase_space_axis_labels('xp',scaleY)},
-                  font_size=18,
-                  transition_duration=500)
-    # return fig
-    return go.Figure(data=[trace_dist,trace], layout=layout)
+    fig.add_traces(data=[trace_dist_x,trace_ell_x],rows=1,cols=1)
+    fig.update_xaxes(title_text=make_phase_space_axis_labels('x',scalex_x),
+                     row=1,
+                     col=1,
+                     titlefont=dict(size=font_size_plot),
+                     title_standoff = x_standoff)
+    fig.update_yaxes(title_text=make_phase_space_axis_labels('xp',scaley_x),
+                     row=1,
+                     col=1,
+                     titlefont=dict(size=font_size_plot),
+                     title_standoff = y_standoff)
+    
+    # ---------------------Y phase space-------------------------
+    trace_ell_y = go.Scatter(
+        x=x_ell_y*10**-scalex_y,
+        y=y_ell_y*10**-scaley_y,
+        marker_color='red',
+        hoverinfo='skip',
+        )
+    
+    trace_dist_y = go.Histogram2d(
+        x=temp_dist[:,2]*10**-scalex_y,
+        y=temp_dist[:,3]*10**-scaley_y,
+        colorscale="Viridis",
+        nbinsx=51,
+        nbinsy=51,
+        showscale=False,
+        hoverinfo='skip',)
+    
+    fig.add_traces(data=[trace_dist_y,trace_ell_y],rows=1,cols=2)
+    fig.update_xaxes(title_text=make_phase_space_axis_labels('y',scalex_y),
+                     row=1,
+                     col=2,
+                     titlefont=dict(size=font_size_plot),
+                     title_standoff = x_standoff)
+    fig.update_yaxes(title_text=make_phase_space_axis_labels('yp',scaley_y),
+                     row=1,
+                     col=2,
+                     titlefont=dict(size=font_size_plot),
+                     title_standoff = y_standoff)
+    
+    # ------------------real space image-------------------------------
+    trace_image = go.Histogram2d(
+        x=temp_dist[:,0]*10**-scalex_x,
+        y=temp_dist[:,2]*10**-scalex_y,
+        colorscale="Viridis",
+        nbinsx=51,
+        nbinsy=51,
+        showscale=False,
+        hoverinfo='skip',)
+    
+    fig.add_traces(data=[trace_image],rows=1,cols=3)
+    fig.update_xaxes(title_text=make_phase_space_axis_labels('x',scalex_x),
+                     row=1,
+                     col=3,
+                     titlefont=dict(size=font_size_plot),
+                     title_standoff = x_standoff)
+    fig.update_yaxes(title_text=make_phase_space_axis_labels('y',scalex_y),
+                     row=1,
+                     col=3,
+                     titlefont=dict(size=font_size_plot),
+                     title_standoff = y_standoff)
+
+    #--------------------overall plot---------------------------------------
+    
+    fig.update_layout(showlegend=False,
+                        autosize=True,
+                        minreducedwidth=250,
+                        minreducedheight=250,
+                        width=1200,
+                        height=450,
+                        font_size=font_size_plot)
+    
+    fig.update_annotations(font_size=font_size_plot)
+    
+    return fig
 
 @app.callback(
-    Output(component_id='emittance-output', component_property='children'),
-    Input('emittance-slider', 'value' ))
-def update_emittance(value):
-    return f"Normalized Emittance (nm-rad): {transform_value(value):.2f}"
+    Output(component_id='emittance-output-x', component_property='children'),
+    Input('emittance-slider-x', 'value' ))
+def update_emittance_x(value):
+    return f"x: {transform_value(value):.1f}"
 
 @app.callback(
-    Output(component_id='alpha-output', component_property='children'),
-    Input('alpha-slider', 'value' ))
-def update_alpha(value):
-    return f"\u03B1: {value:.2f}"
+    Output(component_id='emittance-output-y', component_property='children'),
+    Input('emittance-slider-y', 'value' ))
+def update_emittance_y(value):
+    return f"y: {transform_value(value):.1f}"
 
 @app.callback(
-    Output(component_id='beta-output', component_property='children'),
-    Input('beta-slider', 'value' ))
-def update_beta(value):
-    return f"\u03B2 (m): {transform_value(value):.2f}"
+    Output(component_id='alpha-output-x', component_property='children'),
+    Input('alpha-slider-x', 'value' ))
+def update_alpha_x(value):
+    return f"x: {value:.1f}"
+
+@app.callback(
+    Output(component_id='alpha-output-y', component_property='children'),
+    Input('alpha-slider-y', 'value' ))
+def update_alpha_y(value):
+    return f"y: {value:.1f}"
+
+@app.callback(
+    Output(component_id='beta-output-x', component_property='children'),
+    Input('beta-slider-x', 'value' ))
+def update_beta_x(value):
+    return f"x: {transform_value(value):.3f}"
+
+@app.callback(
+    Output(component_id='beta-output-y', component_property='children'),
+    Input('beta-slider-y', 'value' ))
+def update_beta_y(value):
+    return f"y: {transform_value(value):.3f}"
 
 @app.callback(
     Output(component_id='kinetic-energy-output', component_property='children'),
     Input('kinetic-energy-slider', 'value' ))
 def update_kinetic_energy(value):
-    return f"Kinetic Energy (MeV): {transform_value(value):.2f}"
+    return f"Kinetic Energy (MeV): {transform_value(value):.0f}"
 
 if __name__ == '__main__':
     app.run_server(debug=True)
